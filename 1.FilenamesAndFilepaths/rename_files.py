232a741ce816fd51876b4e1d99c7303646fe3c87
branhoff <branhoff2@gmail.com>
2020-11-18 12:36:36 -0800

    updated files

--- 1.FilenamesAndFilepaths/rename_files.py
+++ 1.FilenamesAndFilepaths/rename_files.py
@@ -1,10 +1,10 @@
-# importing os module 
+# import modules we'll need
 import datetime
 import os
 
 def get_curr_month_name():
     """
-    Pulls curren month as long form name i.e. "January"
+    Pulls current month as long form name i.e. "January"
     """
     today = datetime.date.today()
     curr_month = today.strftime("%B")
@@ -16,14 +16,23 @@ def main():
     for filename in os.listdir("1.FilenamesAndFilepaths\\work_files"):
         print(filename)
 
-    # Steps to think about:
-    # We can iterate through a list of filenames, but we still need to change
+    # What we have:
+    # We can currently iterate through a list of filenames, but we still need to change
     # the names of the "filename" variable. We should remember that strings are immutable
-    # so we can just tell python to cut out the "October" in the filename and replace it with November.
-    # 1. Need to conver the "filename" into something that is mutable i.e. a list
+    # so we can't just tell python to cut out the "October" in the filename and replace it with November.
+
+    # REMEMBER YOU CAN GOOGLE
+    
+    # Steps to think about:
+    # 1. Need to convert the "filename" into something that is mutable (an object that can be changed) i.e. a list
     # 2. Change the part of the mutable that represents "October" to "November"
     # 3. Convert that mutable back to a normal string like before, but now with the correct name
-    # 4. Change the current actual name of the file to our new name
+    # 4. Change the current ACTUAL name of the file to our new name
+
+    # TRY GOOGLING "how to change string to list in python"
+    #              "how to create new folder in python"
+    #              "how to copy files in python"
+    #              "how to rename files in python"
 
 
 # Driver Code 
